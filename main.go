7818c5394a0bd89678bfa613411c1f659cb6cf03
yves848 <yves.godart@gmail.com>
2024-04-30 20:09:17 +0200

    using winget cli

--- main.go
+++ main.go
@@ -2,19 +2,60 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 
-	"github.com/Yves848/wingettui/libs"
 	"github.com/Yves848/wingettui/winget"
 )
 
 func main() {
-	SayHello()
-	libs.SayHello2()
-	out, err := winget.Invoke("list")
-	if err != nil {
-		fmt.Println(errors.New("error invoking winget"))
-	}
-	fmt.Println(string(out))
+	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
+	listSource := listCmd.String("source", "", "source of the package")
 
+	searchCmd := flag.NewFlagSet("search", flag.ExitOnError)
+	searchQuery := searchCmd.String("query", "", "query to search")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'list / search / update' subcommand")
+		os.Exit(1)
+	}
+
+	var params []string = make([]string, 1)
+
+	switch os.Args[1] {
+	case "list":
+		listCmd.Parse(os.Args[2:])
+		fmt.Println("listsource :  " + *listSource)
+
+		params[0] = "-command Get-WinGetPackage | where-object { $_.Source -eq \"winget\"} | ConvertTo-Json -Depth 1 -WarningAction SilentlyContinue"
+		if *listSource != "" {
+
+		}
+
+	case "search":
+		searchCmd.Parse(os.Args[2:])
+		fmt.Println("searchQuery :  " + *searchQuery)
+		params = make([]string, 1)
+		params[0] = "Search-WGPackage \"##QUERY##\" -quiet $true | out-json"
+		if *searchQuery != "" {
+			params[0] = strings.Replace(params[0], "##QUERY##", *searchQuery, -1)
+		}
+	}
+	if params[0] != "" {
+		out, err := winget.Invoke(strings.Join(params, " "))
+		if err != nil {
+			fmt.Println(errors.New("error invoking winget"))
+		}
+		// fmt.Println(string(out))
+		items, err := winget.Winget2Json(string(out))
+		if err != nil {
+			fmt.Println(errors.New("error parsing json"))
+			os.Exit(2)
+		}
+		for _, item := range items.Packages {
+			fmt.Printf("%s %s\n", item.Id, item.InstalledVersion)
+		}
+	}
 }
--- winget/core.go
+++ winget/core.go
@@ -1,15 +1,61 @@
 package winget
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"os/exec"
 	"strings"
 )
 
+type WingetItem struct {
+	AvailableVersions string `json:"AvailableVersions"`
+	Id                string `json:"Id"`
+	Source            string `json:"Source"`
+	InstalledVersion  string `json:"InstalledVersion"`
+	IsUpdateAvailable bool   `json:"IsUpdateAvailable"`
+	Name              string `json:"Name"`
+}
+
+type WingetPackages struct {
+	Packages []WingetItem `json:"packages"`
+}
+
 func Invoke(command string) (res []byte, err error) {
 	cmds := strings.Split(command, " ")
 	fmt.Println("Invoking command: ", command)
-	cmd := exec.Command("winget", cmds...)
-	out, err := cmd.Output()
-	return out, err
+	cmd := exec.Command("pwsh.exe", cmds...)
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("Error creating StdoutPipe: ", err)
+		return
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("Error starting command: ", err)
+		return
+	}
+	var result []byte = make([]byte, 0)
+	scanner := bufio.NewScanner(out)
+	for scanner.Scan() {
+		line := scanner.Text()
+		result = append(result, []byte(line)...)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	return result, err
+}
+
+func Winget2Json(out string) (items WingetPackages, err error) {
+	// fmt.Println(out)
+	js := "{\"packages\":" + out + "}"
+	err = json.Unmarshal([]byte(js), &items)
+	if err != nil {
+		return items, err
+	}
+	return items, nil
 }
